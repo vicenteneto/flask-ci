8900d5501ffc05068f51a45eb381675252d27944
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-03-20 16:30:06 -0300

    Implemented run_nose task

--- flask_ci/tasks/run_nose.py
+++ flask_ci/tasks/run_nose.py
@@ -7,18 +7,24 @@ class Reporter(object):
     def get_arguments():
         return [
             Option('-c', '--with-coverage', action='store_true', dest='with-coverage', default=False),
-            Option('-x', '--with-xunit', action='store_true', dest='with-xunit', default=False)
+            Option('-x', '--with-xunit', action='store_true', dest='with-xunit', default=False),
+            Option('--cover-html', action='store_true', dest='cover-html', default=False),
+            Option('--cover-xml', action='store_true', dest='cover-xml', default=False)
         ]
 
     def run(self, settings, **options):
         args = list()
 
-        if options.get('with-xunit', None):
-            args.append('--with-xunit')
         if options.get('with-coverage', None):
             args.append('--with-coverage')
+        if options.get('with-xunit', None):
+            args.append('--with-xunit')
+        if options.get('cover-html'):
+            args.append('--cover-html')
+        if options.get('cover-xml'):
             args.append('--cover-xml')
-            args.append('--cover-package=%s' % ','.join(getattr(settings, 'PROJECT_APPS', [])))
+
+        args.append('--cover-package=%s' % ','.join(getattr(settings, 'PROJECT_APPS', [])))
 
         args += getattr(settings, 'PROJECT_APPS', [])
 
